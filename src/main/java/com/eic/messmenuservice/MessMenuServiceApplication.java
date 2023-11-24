45184e8b8bb8faa92f7cba7aad4ea5955ff039a8
MnCSSJ4x <Monjoy.Choudhury@iiitb.ac.in>
2023-11-24 20:48:37 +0530

    initial setup

---
+++ src/main/java/com/eic/messmenuservice/MessMenuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eic.messmenuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessMenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessMenuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eic/messmenuservice/MessMenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eic.messmenuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessMenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
