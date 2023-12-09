469d8239ec11184ca83f4b879be8ac25485f768b
MnCSSJ4x <Monjoy.Choudhury@iiitb.ac.in>
2023-12-09 19:11:01 +0530

    updated eureka client

--- src/main/java/com/eic/canteenservice/CanteenserviceApplication.java
+++ src/main/java/com/eic/canteenservice/CanteenserviceApplication.java
@@ -2,9 +2,10 @@ package com.eic.canteenservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class CanteenserviceApplication {
 
 	public static void main(String[] args) {
