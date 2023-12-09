4f8e35a476617ac518bcdfba4b2ab676a028c505
MnCSSJ4x <Monjoy.Choudhury@iiitb.ac.in>
2023-12-09 19:14:09 +0530

    updated eureka client

--- src/main/java/com/eic/foodle/servicerequest/ServiceRequestApplication.java
+++ src/main/java/com/eic/foodle/servicerequest/ServiceRequestApplication.java
@@ -3,8 +3,10 @@ package com.eic.foodle.servicerequest;
 import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceRequestApplication {
 
 	public static void main(String[] args) {
