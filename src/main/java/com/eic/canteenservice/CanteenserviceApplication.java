3522bf2b288dd1e6a5138adcc45a7ee4d5a71480
KMishra23 <kaushik.ultimate96@gmail.com>
2023-12-14 01:52:17 +0530

    logging 5

--- src/main/java/com/eic/canteenservice/CanteenserviceApplication.java
+++ src/main/java/com/eic/canteenservice/CanteenserviceApplication.java
@@ -1,14 +1,21 @@
 package com.eic.canteenservice;
 
+import com.eic.canteenservice.controller.CanteenItemController;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CanteenserviceApplication {
-
+	private static final Logger LOG = LogManager.getLogger(CanteenserviceApplication.class);
 	public static void main(String[] args) {
+		LOG.debug("Started canteen service");
 		SpringApplication.run(CanteenserviceApplication.class, args);
 	}
 
--- src/main/java/com/eic/canteenservice/controller/CanteenItemController.java
+++ src/main/java/com/eic/canteenservice/controller/CanteenItemController.java
@@ -1,5 +1,9 @@
 package com.eic.canteenservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.eic.canteenservice.models.CanteenItem;
 import com.eic.canteenservice.service.CanteenItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,24 +11,30 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/canteen")
 public class CanteenItemController {
+    private static final Logger LOG = LogManager.getLogger(CanteenItemController.class);
+
     @Autowired
     private CanteenItemService service;
 
     @GetMapping
     public List<CanteenItem> getAllFoodItems() {
+        LOG.debug("Getting all items");
         return service.getAllFoodItems();
     }
 
     @GetMapping("/{id}")
     public CanteenItem getFoodItemById(@PathVariable String id) {
+        LOG.debug("Getting item by id");
         return service.getFoodItemById(id);
     }
 
     @PostMapping
     public CanteenItem saveFoodItem(@RequestBody CanteenItem foodItem) {
+        LOG.info("Adding new item to menu");
         return service.saveFoodItem(foodItem);
     }
 
