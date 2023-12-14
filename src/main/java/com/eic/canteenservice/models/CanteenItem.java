8646cedb75f90b64baacfd0f6e4ce3a3e4fa870f
KMishra23 <kaushik.ultimate96@gmail.com>
2023-12-14 14:23:53 +0530

    added 1 testcase for checking

--- src/main/java/com/eic/canteenservice/models/CanteenItem.java
+++ src/main/java/com/eic/canteenservice/models/CanteenItem.java
@@ -3,9 +3,13 @@ package com.eic.canteenservice.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "canteen_food_items")
--- src/test/java/com/eic/canteenservice/CanteenserviceApplicationTests.java
+++ src/test/java/com/eic/canteenservice/CanteenserviceApplicationTests.java
@@ -1,13 +1,48 @@
 package com.eic.canteenservice;
 
+import com.eic.canteenservice.repository.CanteenItemRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.eic.canteenservice.CanteenserviceApplication;
+import com.eic.canteenservice.service.CanteenItemService;
+import com.eic.canteenservice.controller.CanteenItemController;
+import com.eic.canteenservice.models.CanteenItem;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest(classes =  CanteenserviceApplication.class)
+@AutoConfigureMockMvc
 class CanteenserviceApplicationTests {
+	@Mock
+	private CanteenItemRepository canteenItemRepository;
+
+	@InjectMocks
+	private CanteenItemController canteenItemController;
+
+	public void CanteenControllerTests(){
+		MockitoAnnotations.openMocks(this);
+	}
 
 	@Test
-	void contextLoads() {
+	void testGetId(){
+		CanteenItem item = new CanteenItem();
+		item.setId("T1");
+
+		assertEquals("T1", item.getId());
 	}
+
+
+//	@Test
+//	void testGetAllFoodItems() {
+//		CanteenItem item1 = new CanteenItem();
+//		item1.setid("T1");
+//		item1.settitle("testName");
+//		item1.setdescription("test description")
+//	}
 
 }
